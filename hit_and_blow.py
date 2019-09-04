e57331f0110b0beeea3ed8d366845bc424f182b6
nakano96 <hideya.nakano.1996@gmail.com>
2019-09-04 22:17:12 +0900

    generate_answerバグ修正

--- hit_and_blow.py
+++ hit_and_blow.py
@@ -1,4 +1,3 @@
-import unittest
 from typing import List
 
 
@@ -13,33 +12,35 @@ def count_duplicates(lst: List):
 class HitAndBlow:
     _len_answer = 4
 
-    def __init__(self, answer: int = 0):
-        self._numbers = list(range(1, 10))
+    def __init__(self, answer_: int = 0):
+        self._numbers = [str(n) for n in range(1, 10)]
         self.is_over = False
         self.count_tries = 0
-        if answer == 0:
+        if answer_ == 0:
             self._correct_answer = self._generate_answer()
         else:
-            self._correct_answer = list(str(answer))
+            self._correct_answer = list(str(answer_))
 
     def _generate_answer(self) -> List:
         import random
         return random.sample(self._numbers, k=self._len_answer)
 
     def score(self, int_answer: int):
-        answer = list(str(int_answer))
-        hit_ = len([i for i, j in zip(answer, self._correct_answer) if i == j])
-        blow_ = count_duplicates(answer + self._correct_answer) - hit_
+        answer_ = list(str(int_answer))
+        hit_ = len([i for i, j in zip(answer_, self._correct_answer) if i == j])
+        blow_ = count_duplicates(answer_ + self._correct_answer) - hit_
         return "{0}H{1}B".format(hit_, blow_)
 
-    def input(self, input_: str):
-        if input_ == "A":
+    def answer(self, input_: str):
+        if input_ in ["", "?", "H"]:
+            pass  # todo:マニュアル返す
+        elif input_ == "A":
             return "Correct answer is {}".format(self._correct_answer)
         elif input_.isnumeric() and len(input_) == self._len_answer:
-            score = self.score(int(input_))
+            score_ = self.score(int(input_))
             self.count_tries += 1
-            self.is_over = (score == "4H0B")
-            return score
+            self.is_over = (score_ == "4H0B")
+            return score_
         else:
             return "Input {}-digit number.".format(self._len_answer)
 
@@ -51,39 +52,11 @@ class GameConsole:
 
     def main(self):
         while True:
-            score = self.game.input(input())
+            score = self.game.answer(input())
             print(score)
             if self.game.is_over:
                 break
 
 
-class TestDefDuplicates(unittest.TestCase):
-    def test_count_duplicates(self):
-        expected = 1
-        actual = count_duplicates([1, 2, 3, 1])
-        self.assertEqual(expected, actual)
-
-    def test_has_duplicates(self):
-        expected = True
-        actual = has_duplicates([1, 2, 3, 1])
-        self.assertEqual(expected, actual)
-
-
-class TestHitAndBlow(unittest.TestCase):
-    def test_score(self):
-        self.game = HitAndBlow(1234)
-        expect = "1H1B"
-        actual = self.game.score(1389)
-        self.assertEqual(expect, actual)
-
-    def test_input(self):
-        self.game = HitAndBlow(1234)
-        expect = "0H4B"
-        actual = self.game.input("4321")
-        self.assertEqual(expect, actual)
-
-
 if __name__ == "main":
-    a = GameConsole()
-    print(a)
-    pass
+    GameConsole()
---
+++ test_hit_and_blow.py
@@ -0,0 +1,29 @@
+import unittest
+from hit_and_blow import count_duplicates, has_duplicates, HitAndBlow, GameConsole
+
+
+class TestDefDuplicates(unittest.TestCase):
+    def test_count_duplicates(self):
+        expected = 1
+        actual = count_duplicates([1, 2, 3, 1])
+        self.assertEqual(expected, actual)
+
+    def test_has_duplicates(self):
+        expected = True
+        actual = has_duplicates([1, 2, 3, 1])
+        self.assertEqual(expected, actual)
+
+
+class TestHitAndBlow(unittest.TestCase):
+    def test_score(self):
+        self.game = HitAndBlow(1234)
+        expect = "1H1B"
+        actual = self.game.score(1389)
+        self.assertEqual(expect, actual)
+
+    def test_answer(self):
+        self.game = HitAndBlow(1234)
+        expect = "0H4B"
+        actual = self.game.answer("4321")
+        self.assertEqual(expect, actual)
+
