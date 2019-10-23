428ffc6938c18eb04622cec3a488a85cfd9afb91
nakano96 <hideya.nakano.1996@gmail.com>
2019-10-24 00:59:40 +0900

    重複を許せるようにした。

--- hit_and_blow.py
+++ hit_and_blow.py
@@ -1,10 +1,6 @@
 from typing import List
 
 
-def has_duplicate(lst: List):
-    return count_duplicates(lst) > 0
-
-
 def count_duplicates(lst: List):
     return len(lst) - len(set(lst))
 
@@ -20,29 +16,46 @@ class HitAndBlow:
     class LengthError(Exception):
         pass
 
-    def __init__(self, len_answer, answer_: int = 0):
+    def __init__(self, len_answer: int = 4, correct_answer: int = 0,
+                 min_: int = 1, max_: int = 9,
+                 allows_duplicate: bool = False):
+        # rules
         self._len_answer = len_answer
-        self._1_digit_numbers = [str(n) for n in range(1, 10)]
+        self._allows_duplicate = allows_duplicate
+        self._1_digit_numbers = self._get_numbers_list(min_, max_)
+        # score
         self._hit = 0
         self._blow = 0
         self._is_over = False
-        self._count_tries = 0
-        if answer_:
-            self._valid_answer(str(answer_))
-            self._correct_answer = list(str(answer_))
+        self._answers = []
+        # answer
+        if correct_answer:
+            self._valid_answer(str(correct_answer))
+            self._correct_answer = list(str(correct_answer))
         else:
             self._correct_answer = self._generate_answer()
 
+    @staticmethod
+    def _get_numbers_list(min_: int, max_: int):
+        if min_ < max_ and {min_, max_} <= set(range(0, 10)):  # 部分集合か?
+            return [str(n) for n in range(min_, max_ + 1)]
+        else:
+            raise ValueError()
+
     def _generate_answer(self) -> List:
         import random
-        return random.sample(self._1_digit_numbers, k=self._len_answer)
+        if self._allows_duplicate:
+            fn = random.sample
+        else:
+            fn = random.choices
+        return fn(self._1_digit_numbers, k=self._len_answer)
 
     def _score(self, answer_: str):
-        answer_ = list(answer_)
-        self._hit = len([i for i, j in zip(answer_, self._correct_answer) if i == j])
-        self._blow = count_duplicates(answer_ + self._correct_answer) - self._hit
+        self._valid_answer(answer_)
+        self._hit = len([i for i, j in zip(list(answer_), self._correct_answer) if i == j])
+        self._blow = count_duplicates(list(answer_) + self._correct_answer) - self._hit
         self._is_over = (self._hit == self._len_answer)
-        self._count_tries += 1
+        self._answers.append(answer_)
 
     @property
     def is_over(self):
@@ -53,22 +66,21 @@ class HitAndBlow:
             raise self.NotNumericError()
         if len(answer_) != self._len_answer:
             raise self.LengthError()
-        if has_duplicate(list(answer_)):
+        if not self._allows_duplicate and count_duplicates(list(answer_)):
             raise self.DuplicationError()
 
     def answer(self, input_: str):
         input_ = input_.upper()
         try:
-            self._valid_answer(input_)
             self._score(input_)
-            return "[{2}]{0}H{1}B".format(self._hit, self._blow, self._count_tries)
+            return "[{2}]{0}H{1}B".format(self._hit, self._blow, len(self._answers))
         except self.DuplicationError:
             return "Please enter a unique number."
         except (self.NotNumericError, self.LengthError):
             if input_ in ["", "?", "H"]:
-                pass  # todo:マニュアル返す
+                return "Wikipedia: 'https://en.wikipedia.org/wiki/Mastermind_(board_game)'"
             elif input_ == "A":
-                return "Correct [A]nswer is {}".format(self._correct_answer)
+                return "Correct [A]nswer is {}.".format(self._correct_answer)
             else:
                 return "Please enter a {}-digit number.".format(self._len_answer)
 
@@ -85,4 +97,4 @@ class GameConsole:
 
 
 if __name__ == '__main__':
-    GameConsole(HitAndBlow(4))
+    GameConsole(HitAndBlow())
--- test_hit_and_blow.py
+++ test_hit_and_blow.py
@@ -1,16 +1,11 @@
 import unittest
-from hit_and_blow import count_duplicates, has_duplicate, HitAndBlow
+from hit_and_blow import count_duplicates, HitAndBlow
 
 
 class TestDefDuplicates(unittest.TestCase):
     def test_count_duplicates(self):
         expected = 1
         actual = count_duplicates([1, 2, 3, 1, 1])
-        self.assertEqual(expected, actual)
-
-    def test_has_duplicates(self):
-        expected = True
-        actual = has_duplicate([1, 2, 3, 1])
         self.assertEqual(expected, actual)
 
 
@@ -33,3 +28,14 @@ class TestHitAndBlow(unittest.TestCase):
 
         with self.assertRaises(HitAndBlow.DuplicationError):
             HitAndBlow(4, 1233)
+
+        with self.assertRaises(ValueError):
+            HitAndBlow(min_=1, max_=1)
+
+        with self.assertRaises(ValueError):
+            HitAndBlow(max_=10)
+
+        game = HitAndBlow(correct_answer=33, allows_duplicate=True, len_answer=2)
+
+        self.assertEqual("[1]1H1B", game.answer("34"))
+        self.assertEqual("[1]1H1B", game.answer("34"))
